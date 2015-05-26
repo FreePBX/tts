adabf14cb7781c47146ae7bf85b37ad63da3df4b
Bryan Walters <bwalters@sangoma.com>
2015-05-26 18:24:29 -0400

    Use correct html to get the same style as the rest of FreePBX

--- views/tts.php
+++ views/tts.php
@@ -18,110 +18,126 @@
 					<?php if ($id) { ?>
 						<input type="hidden" name="id" value="<?php echo $id; ?>">
 					<?php } ?>
-					<!--Device List-->
-					<div class="element-container">
-						<div class="row">
-							<div class="form-group">
+
+					<?php //Main Settings ?>
+					<div class="section-title" data-for="section1"><h3><i class="fa fa-minus"></i><?php echo _("Main settings"); ?></h3></div>
+					<div class="section" data-id="section1">
+						<div class="element-container">
+							<div class="row">
 								<div class="col-md-9">
-									<h5><?php echo _("Main settings"); ?>:<hr></h5>
+									<div class="row">
+										<div class="form-group">
+											<div class="col-md-3">
+												<label class="control-label" for="name"><?php echo _("Name"); ?></label>
+												<i class="fa fa-question-circle fpbx-help-icon" data-for="name"></i>
+											</div>
+											<div class="col-md-9"><input type="text" class="form-control" name="name" value="<?php echo (isset($name) ? $name : ''); ?>"></div>
+										</div>
+									</div>
+								</div>
+							</div>
+							<div class="row">
+								<div class="col-md-12">
+									<span id="name-help" class="help-block fpbx-help-block"><?php echo _("Give this TTS Destination a brief name to help you identify it"); ?></span>
 								</div>
 							</div>
 						</div>
-						<div class="row">
-							<div class="form-group">
-								<div class="col-md-3">
-									<label class="control-label" for="name"><?php echo _("Name"); ?></label>
-									<i class="fa fa-question-circle fpbx-help-icon" data-for="name"></i>
-								</div>
-								<div class="col-md-6">
-									<input type="text" name="name" value="<?php echo (isset($name) ? $name : ''); ?>">
-								</div>
-							</div>
-						</div>
-						<div class="row">
-							<div class="col-md-12">
-								<span id="name-help" class="help-block fpbx-help-block"><?php echo _("Give this TTS Destination a brief name to help you identify it"); ?></span>
-							</div>
-						</div>
-						<div class="row">
-							<div class="form-group">
-								<div class="col-md-3">
-									<label class="control-label" for="text"><?php echo _("Text"); ?></label>
-									<i class="fa fa-question-circle fpbx-help-icon" data-for="text"></i>
-								</div>
-								<div class="col-md-6">
-									<textarea name="text" cols=50 rows=5>
-										<?php echo (isset($text) ? $text : ''); ?>
-									</textarea>
-								</div>
-							</div>
-						</div>
-						<div class="row">
-							<div class="col-md-12">
-								<span id="text-help" class="help-block fpbx-help-block"><?php echo _("Enter the text you want to synthetize."); ?></span>
-							</div>
-						</div>
-						<div class="row">
-							<div class="form-group">
+						<div class="element-container">
+							<div class="row">
 								<div class="col-md-9">
-									<h5><?php echo _("TTS Engine"); ?>:<hr></h5>
+									<div class="row">
+										<div class="form-group">
+											<div class="col-md-3">
+												<label class="control-label" for="name"><?php echo _("Text"); ?></label>
+												<i class="fa fa-question-circle fpbx-help-icon" data-for="text"></i>
+											</div>
+											<div class="col-md-9">
+												<textarea name="text" class="form-control" cols=50 rows=5>
+													<?php echo (isset($text) ? $text : ''); ?>
+												</textarea>
+											</div>
+										</div>
+									</div>
 								</div>
 							</div>
-						</div>
-						<div class="row">
-							<div class="form-group">
-								<div class="col-md-3">
-									<label class="control-label" for="engine"><?php echo _("Choose an Engine"); ?></label>
-									<i class="fa fa-question-circle fpbx-help-icon" data-for="engine"></i>
+							<div class="row">
+								<div class="col-md-12">
+									<span id="text-help" class="help-block fpbx-help-block"><?php echo _("Enter the text you want to synthetize."); ?></span>
 								</div>
-								<div class="col-md-6">
-									<?php if( !isset($tts_agi_error) ) { ?>
-										<select name="engine">
-											<?php
-												foreach ($engine_list as $e)
-												{
-													if ($e['name'] == $engine) {
-														echo '<option value="' . $e['name'] . '" selected=1>' . $e['name'] . '</option>';
-													} else {
-														echo '<option value="' . $e['name'] . '">' . $e['name'] . '</option>';
-													}
-												}
-											?>
-										</select>
-									<?php } else { ?>
-										<i><?php echo $tts_agi_error; ?></i>
-									<?php } ?>
-								</div>
-							</div>
-						</div>
-						<div class="row">
-							<div class="col-md-12">
-								<span id="engine-help" class="help-block fpbx-help-block"><?php echo _("List of TTS engine detected on the server. Choose the one you want to use for the current sentence."); ?></span>
-							</div>
-						</div>
-						<div class="row">
-							<div class="form-group">
-								<div class="col-md-3">
-									<label class="control-label" for="goto"><?php echo _("Desintation"); ?></label>
-									<i class="fa fa-question-circle fpbx-help-icon" data-for="goto"></i>
-								</div>
-								<div class="col-md-6">
-									<?php
-									if (isset($goto)) {
-										echo drawselects($goto,0);
-									} else {
-										echo drawselects(null, 0);
-									}
-									?>
-								</div>
-							</div>
-						</div>
-						<div class="row">
-							<div class="col-md-12">
-								<span id="goto-help" class="help-block fpbx-help-block"><?php echo _("After the Text to Speech was played go to"); ?></span>
 							</div>
 						</div>
 					</div>
+
+					<?php //TTS Engine ?>
+					<div class="section-title" data-for="section2"><h3><i class="fa fa-minus"></i><?php echo _("TTS Engines"); ?></h3></div>
+					<div class="section" data-id="section2">
+						<div class="element-container">
+							<div class="row">
+								<div class="col-md-9">
+									<div class="row">
+										<div class="form-group">
+											<div class="col-md-3">
+												<label class="control-label" for="engine"><?php echo _("Choose an Engine"); ?></label>
+												<i class="fa fa-question-circle fpbx-help-icon" data-for="engine"></i>
+											</div>
+											<div class="col-md-9">
+												<?php if( !isset($tts_agi_error) ) { ?>
+													<select name="engine" class="form-control">
+														<?php
+															foreach ($engine_list as $e)
+															{
+																if ($e['name'] == $engine) {
+																	echo '<option value="' . $e['name'] . '" selected=1>' . $e['name'] . '</option>';
+																} else {
+																	echo '<option value="' . $e['name'] . '">' . $e['name'] . '</option>';
+																}
+															}
+														?>
+													</select>
+												<?php } else { ?>
+													<i><?php echo $tts_agi_error; ?></i>
+												<?php } ?>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+							<div class="row">
+								<div class="col-md-12">
+									<span id="engine-help" class="help-block fpbx-help-block"><?php echo _("List of TTS engine detected on the server. Choose the one you want to use for the current sentence."); ?></span>
+								</div>
+							</div>
+						</div>
+						<div class="element-container">
+							<div class="row">
+								<div class="col-md-9">
+									<div class="row">
+										<div class="form-group">
+											<div class="col-md-3">
+												<label class="control-label" for="goto"><?php echo _("Desintation"); ?></label>
+												<i class="fa fa-question-circle fpbx-help-icon" data-for="goto"></i>
+											</div>
+											<div class="col-md-9">
+												<?php
+												if (isset($goto)) {
+													echo drawselects($goto,0);
+												} else {
+													echo drawselects(null, 0);
+												}
+												?>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+							<div class="row">
+								<div class="col-md-12">
+									<span id="goto-help" class="help-block fpbx-help-block"><?php echo _("After the Text to Speech was played go to"); ?></span>
+								</div>
+							</div>
+						</div>
+					</div>
+					<?php //END TTS Engines ?>
 					</form>
 				</div>
 			</div>
