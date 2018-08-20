cabf89c07ef1a00207caf9ca78de4cfb99cd0cc6
James Finstrom <jfinstrom@sangoma.com>
2018-08-20 17:42:57 +0000

    Fix Syntax error

--- Restore.php
+++ Restore.php
@@ -11,7 +11,7 @@ class Restore Extends Base\RestoreBase{
 
   public function processLegacy($pdo, $data, $tables, $unknownTables, $tmpfiledir){
     $tables = array_flip($tables+$unknownTables);
-    if(!isset(tables['tts'])){
+    if(!isset($tables['tts'])){
       return $this;
     }
     $bmo = $this->FreePBX->Tts;
