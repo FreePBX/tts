e617bb24cfe1a8d81b45a24bc25db5c00e6e07ef
Andrew Nagy <andrew.nagy@the159.com>
2019-02-21 23:42:11 -0500

    [Module Tag script: tts 15.0.6] fixing legacy restore

--- Backup.php
+++ Backup.php
@@ -2,9 +2,9 @@
 namespace FreePBX\modules\Tts;
 use FreePBX\modules\Backup as Base;
 class Backup Extends Base\BackupBase{
-  public function runBackup($id,$transaction){
-    $configs = $this->FreePBX->Tts->listTTS();
-    $this->addDependency('ttsengines');
-    $this->addConfigs($configs);
-  }
+	public function runBackup($id,$transaction){
+		$this->addDependency('ttsengines');
+		$configs = $this->dumpTables();
+		$this->addConfigs($configs);
+	}
 }
--- Restore.php
+++ Restore.php
@@ -2,26 +2,13 @@
 namespace FreePBX\modules\Tts;
 use FreePBX\modules\Backup as Base;
 class Restore Extends Base\RestoreBase{
-  public function runRestore($jobid){
-    $configs = $this->getConfigs();
-    foreach ($configs as $tts) {
-        $this->FreePBX->Tts->add($tts['name'], $tts['text'], $tts['goto'], $tts['engine']);
-    }
-  }
+	public function runRestore($jobid){
+		$configs = $this->getConfigs();
+		$this->importTables($configs);
+	}
 
-  public function processLegacy($pdo, $data, $tables, $unknownTables, $tmpfiledir){
-    $tables = array_flip($tables+$unknownTables);
-    if(!isset($tables['tts'])){
-      return $this;
-    }
-    $bmo = $this->FreePBX->Tts;
-    $bmo->setDatabase($pdo);
-    $configs = $bmo->listTTS();
-    $bmo->resetDatabase();
-    foreach ($configs as $tts) {
-      $bmo->add($tts['name'], $tts['text'], $tts['goto'], $tts['engine']);
-    }
-    return $this;
-  }
+	public function processLegacy($pdo, $data, $tables, $unknownTables){
+		$this->restoreLegacyDatabase($pdo);
+	}
 
 }
