dc455baaf503b71bd0ea298aaa8a4659586cd2cb
Prasanth Mohan <prasathm@sangoma.com>
2022-05-12 11:50:59 +0000

    FREEI-4793 Fixed styling issues on text to speech module

--- views/tts.php
+++ views/tts.php
@@ -26,8 +26,8 @@
 						<div class="element-container">
 							<div class="row">
 								<div class="col-md-9">
-									<div class="row">
-										<div class="form-group">
+									<div class="">
+										<div class="row form-group">
 											<div class="col-md-3">
 												<label class="control-label" for="name"><?php echo _("Name"); ?></label>
 												<i class="fa fa-question-circle fpbx-help-icon" data-for="name"></i>
@@ -46,8 +46,8 @@
 						<div class="element-container">
 							<div class="row">
 								<div class="col-md-9">
-									<div class="row">
-										<div class="form-group">
+									<div class="">
+										<div class="row form-group">
 											<div class="col-md-3">
 												<label class="control-label" for="name"><?php echo _("Text"); ?></label>
 												<i class="fa fa-question-circle fpbx-help-icon" data-for="text"></i>
@@ -73,8 +73,8 @@
 						<div class="element-container">
 							<div class="row">
 								<div class="col-md-9">
-									<div class="row">
-										<div class="form-group">
+									<div class="">
+										<div class="row form-group">
 											<div class="col-md-3">
 												<label class="control-label" for="engine"><?php echo _("Choose an Engine"); ?></label>
 												<i class="fa fa-question-circle fpbx-help-icon" data-for="engine"></i>
@@ -110,8 +110,8 @@
 						<div class="element-container">
 							<div class="row">
 								<div class="col-md-9">
-									<div class="row">
-										<div class="form-group">
+									<div class="">
+										<div class="row form-group">
 											<div class="col-md-3">
 												<label class="control-label" for="goto"><?php echo _("Destintation"); ?></label>
 												<i class="fa fa-question-circle fpbx-help-icon" data-for="goto"></i>
